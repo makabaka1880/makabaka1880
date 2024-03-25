cd9339758ccdc9f49768e9d58629fec829317739
makabaka1880 <makabaka1880@outlook.com>
2024-03-26 05:49:40 +0800

    added navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,26 @@ module.exports = {
   themeConfig: {
     logo: "https://img2.imgtp.com/2024/03/24/DUQvvzXV.png",
     displayAllHeaders: true,
+    nav: [
+      {
+        text: "首页",
+        link: "/"
+      },
+      {
+        text: "CS",
+        link: "/CS/",
+        items: [
+          { text: "CASIO", link: "/CS/Casio"}
+        ]
+      },
+      {
+        text: "精神分析",
+        link: "/Psychoanalysis/",
+        items: [
+          { text: "精神分析学导论", link: "/Psychoanalysis/Psych"}
+        ]
+      }
+    ],
     sidebar: [
       {
         title: '首页',   // required
