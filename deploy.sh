9705deb1ec5888ed666c97d2ac810d62afcef655
makabaka1880 <makabaka1880@outlook.com>
2024-03-28 18:54:40 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
-set -e
+# # 确保脚本抛出遇到的错误
+# set -e
 
+# # # 生成静态文件
+# # yarn docs:build
+
+# cd ~/blog-archiac
 # # 生成静态文件
 # yarn docs:build
+# # 进入生成的文件夹
+# cd docs/.vuepress/dist
 
-cd ~/blog-archiac
-# 生成静态文件
-yarn docs:build
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-git init
+# git init
 git remote add origin git@github.com:makabaka1880/makabaka1880.github.io.git
 git add -A
 echo "ready to deploy"
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,17 +14,6 @@ module.exports = {
     ['@vuepress/back-to-top'],
     ['@vuepress/container'],
     ['autonav', true],
-    [
-      'vuepress-plugin-graypage', {
-        // 是否开启控制台日志打印(default: false)
-        log: true,
-        // 月+日(哀悼日生效日期)
-        mournDay: ['3-28'],
-        // 年+月+日(哀悼日生效日期)
-        // 特殊的日期(例如：清明节等...建议使用默认即可)
-        special: ['banBenLongYi']
-      }
-    ],
     ['vuepress-plugin-nprogress'],
     'vuepress-plugin-flowchart',
     [
@@ -32,6 +21,10 @@ module.exports = {
         hostname: 'https://localhost:8080'
       },
     ],
+    ['graysite',{
+      startDate: '2020-04-03 00:00:00',
+      endDate: '2025-04-04 23:59:59'
+    }],
     [
        'dynamic-title',
        {
