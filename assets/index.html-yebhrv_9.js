13a641b435fea644d7a27c1c424016652e954d3c
makabaka1880 <makabaka1880@outlook.com>
2025-06-17 19:58:11 +0800

    deploy

---
+++ assets/index.html-yebhrv_9.js
@@ -0,0 +1,26 @@
+import{_ as l,c as i,a as o,b as e,d as r,e as s,r as p,o as c,f as n}from"./app-DLOF5IZh.js";const m={};function d(u,t){const a=p("center");return c(),i("div",null,[o(` 
+  - text: 一台UAV的结构👉
+    link: ./CS/UAV
+    icon: signs-post
+    type: primary
+
+  - text: 博客
+    icon: lightbulb
+    link: ./article/
+
+  - text: 大事记
+    icon: star
+    link: ./timeline/
+
+  - text: 友链
+    icon: heart
+    link: ./CS/Friends
+
+- title: 理科笔记
+  details: 计算机科学以及数学一直是我很喜欢的两门门学科,我现在也在往我的博客上面搬CS和数学笔记了 还没搬完
+- title: 精神分析学
+  details: 我也是因为一些自身原因接触到心理学和精神病学的。当然我现在有这坨笔记也多亏了我当初脑抽选读了弗洛伊德的讲义
+- title: 自然科学
+  details: 最近搞生赛，资料也传上来了。全部笔记包括物理, 化学, 生物, etc... 待完善
+- title: 友链
+  details: 欢迎来CS/Friends提交友链呀 `),t[1]||(t[1]=e("div",{class:"hint-container warning"},[e("p",{class:"hint-container-title"},"告示"),e("p",null,"尊敬的各位访客："),e("p",null,"我的父亲于2024年12月20日不幸去世，年仅52岁。由于还未尸检，死亡原因尚不明确，但大概率是噎食导致的机械性窒息。他的生命永远定格在那个冬日，但他留给我们的爱与回忆却永远不会褪色。"),e("p",null,"就在父亲去世的前一天，我收到了上海新河湾双语的预录取通知。至少在那一刻，他是高兴的，我也会带着这份喜悦和他的期盼继续前行。"),e("p",null,"我们刚刚从公安局取回了他的手机。翻看他的日常开销，除了工作和吃饭，几乎全部是转给家人的零花钱。他的一生，作为父亲和家庭的支柱，已将他的爱给予得如此饱满。"),e("p",null,"为表达我们的深切哀思，本墓碑将调整为黑白模式。感谢您的理解与支持。"),e("p",null,"为表达我们的深切哀思，本网站将暂时调整为黑白模式。感谢您的理解与支持。"),e("p",null,"愿爱与记忆永存。")],-1)),r(a,null,{default:s(()=>t[0]||(t[0]=[e("hr",null,null,-1),e("p",null,[e("strong",null,"你好陌生人👋～")],-1),e("p",null,"我是一名普普通通的上海初中牲",-1),e("p",null,[n("在学习各种奇奇怪怪的东西的时候也会时常有各类笔记"),e("br"),n(" 之前一只找不到很好的整理方式，正好最近想整个博客，所以也就把所有东西搬上来了")],-1)])),_:1})])}const h=l(m,[["render",d],["__file","index.html.vue"]]),g=JSON.parse('{"path":"/","title":"","lang":"zh-CN","frontmatter":{"home":true,"layout":"BlogHome","heroText":"Makabaka1880的博客 | 中考季 :(","heroImage":"/assets/Hero.webp","bgImage":"/BG-HW.webp","heroFullScreen":true,"tagline":"欢迎来到Makabaka1880的博客👏","footer":"MIT Licensed | Copyright 2024 © Sean Li","actions":[{"text":"电子墓碑 →","link":"https://www.jamesli.love"}],"features":[{"title":"2024/12/20","details":"老爸一路走好"}],"description":"告示 尊敬的各位访客： 我的父亲于2024年12月20日不幸去世，年仅52岁。由于还未尸检，死亡原因尚不明确，但大概率是噎食导致的机械性窒息。他的生命永远定格在那个冬日，但他留给我们的爱与回忆却永远不会褪色。 就在父亲去世的前一天，我收到了上海新河湾双语的预录取通知。至少在那一刻，他是高兴的，我也会带着这份喜悦和他的期盼继续前行。 我们刚刚从公安局取回...","head":[["link",{"rel":"alternate","hreflang":"en-us","href":"https://makabaka1880.xyz/en/"}],["meta",{"property":"og:url","content":"https://makabaka1880.xyz/"}],["meta",{"property":"og:site_name","content":"Makabaka1880的博客"}],["meta",{"property":"og:description","content":"告示 尊敬的各位访客： 我的父亲于2024年12月20日不幸去世，年仅52岁。由于还未尸检，死亡原因尚不明确，但大概率是噎食导致的机械性窒息。他的生命永远定格在那个冬日，但他留给我们的爱与回忆却永远不会褪色。 就在父亲去世的前一天，我收到了上海新河湾双语的预录取通知。至少在那一刻，他是高兴的，我也会带着这份喜悦和他的期盼继续前行。 我们刚刚从公安局取回..."}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2025-03-22T03:53:16.000Z"}],["meta",{"property":"article:modified_time","content":"2025-03-22T03:53:16.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"\\",\\"description\\":\\"告示 尊敬的各位访客： 我的父亲于2024年12月20日不幸去世，年仅52岁。由于还未尸检，死亡原因尚不明确，但大概率是噎食导致的机械性窒息。他的生命永远定格在那个冬日，但他留给我们的爱与回忆却永远不会褪色。 就在父亲去世的前一天，我收到了上海新河湾双语的预录取通知。至少在那一刻，他是高兴的，我也会带着这份喜悦和他的期盼继续前行。 我们刚刚从公安局取回...\\"}"]]},"headers":[],"git":{"createdTime":1734876375000,"updatedTime":1742615596000,"contributors":[{"name":"makabaka1880","username":"makabaka1880","email":"makabaka1880@outlook.com","commits":8,"url":"https://github.com/makabaka1880"}]},"readingTime":{"minutes":2.04,"words":613},"filePathRelative":"README.md","localizedDate":"2024年12月22日","excerpt":"<!-- \\n  - text: 一台UAV的结构👉\\n    link: ./CS/UAV\\n    icon: signs-post\\n    type: primary\\n\\n  - text: 博客\\n    icon: lightbulb\\n    link: ./article/\\n\\n  - text: 大事记\\n    icon: star\\n    link: ./timeline/\\n\\n  - text: 友链\\n    icon: heart\\n    link: ./CS/Friends\\n\\n- title: 理科笔记\\n  details: 计算机科学以及数学一直是我很喜欢的两门门学科,我现在也在往我的博客上面搬CS和数学笔记了 还没搬完\\n- title: 精神分析学\\n  details: 我也是因为一些自身原因接触到心理学和精神病学的。当然我现在有这坨笔记也多亏了我当初脑抽选读了弗洛伊德的讲义\\n- title: 自然科学\\n  details: 最近搞生赛，资料也传上来了。全部笔记包括物理, 化学, 生物, etc... 待完善\\n- title: 友链\\n  details: 欢迎来CS/Friends提交友链呀 -->","autoDesc":true}');export{h as comp,g as data};
